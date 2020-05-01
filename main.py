ffa3f8c4eb0af7077efbfe695db88bd32741eae9
dposlt <david.poslt@gmail.com>
2020-05-01 17:43:50 +0200

    prin cell:

--- main.py
+++ main.py
@@ -7,31 +7,17 @@ def open_file(path):
     Open and read an Excel file
     """
     book = xlrd.open_workbook(path)
-    
-    # print number of sheets
-    print(book.nsheets)
-    
-    # print sheet names
-    print(book.sheet_names())
-    
+
     # get the first worksheet
-    first_sheet = book.sheet_by_index(0)
-    
-    # read a row
-    print(first_sheet.row_values(0))
-    
-    # read a cell
-    cell = first_sheet.cell(0,0)
-    print(cell)
-    print(cell.value)
-    
-    # read a row slice
-    print(first_sheet.row_slice(rowx=0,
-                                start_colx=0,
-                                end_colx=2))
-    
+    sheets = book.sheet_by_index(0)
+
+
+    for cell in sheets.row(2):
+        print(cell.value)
+
+
 #----------------------------------------------------------------------
 if __name__ == "__main__":
-    path = "sheets/hdo.xls"
+    path = "sheets/d.xls"
     open_file(path)
 
